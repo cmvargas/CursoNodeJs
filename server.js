d3beb65b164002afc47f4df03c635c8ddaa682f6
cmvargas <cmvargase@hotmail.com>
2019-10-03 22:49:40 -0500

    REcibiendo peticiones query y body

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(router);
+
 // app.use('/', (req,res)=>{
 //   res.send('Hola');
 // });
@@ -10,6 +14,11 @@ router.get('/message',(req,res)=>{
 });
 router.post('/message',(req,res)=>{
   res.send('Mensaje añadido');
+});
+router.delete('/message',(req,res)=>{
+  console.log(req.query);
+  console.log(req.body);
+  res.send('Mensaje '+req.body.text+'eliminado ya');
 });
 app.listen(3000,()=>{
   console.log('Escuchando en el puerto 3000');
