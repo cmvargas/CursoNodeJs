6e580488e3f49ca314cf7404e71682c2897ae886
cmvargas <cmvargase@hotmail.com>
2019-10-05 19:23:15 -0500

    Mejorar la presentacion de respuestas

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const response = require('./network/response.js');
 const router = express.Router();
 var app = express();
 app.use(bodyParser.json());
@@ -10,16 +11,28 @@ app.use(router);
 //   res.send('Hola');
 // });
 router.get('/message',(req,res)=>{
-  res.send('Lista de mensajes');
+  console.log(req.headers);
+  res.header({
+    "custom-header": "Nuestro valor personalizado"
+  });
+  response.success(req, res, 'Lista de mensajes',201);
 });
 router.post('/message',(req,res)=>{
-  res.send('Mensaje añadido');
+  console.log(req.query.error=="ok");
+  if(req.query.error == "ok"){
+    response.error(req, res, 'Error inesperado',500, 'Es solo una simulacion de los errores');
+  }else{
+    response.success(req, res, 'Creado correctamente',201);
+  }
+  //res.status(201).send({error: '',body: 'Creado correctamente'});
+
 });
 router.delete('/message',(req,res)=>{
   console.log(req.query);
   console.log(req.body);
-  res.send('Mensaje '+req.body.text+'eliminado ya');
+  res.status(201).send();
 });
+app.use('/app', express.static('public'));
 app.listen(3000,()=>{
   console.log('Escuchando en el puerto 3000');
 });
