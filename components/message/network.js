479366ba95b7b01e9ece1a489b76b026da6afc03
cmvargas <cmvargase@hotmail.com>
2019-10-07 22:50:45 -0500

    Realizando controller y store del api

--- components/message/network.js
+++ components/message/network.js
@@ -1,21 +1,24 @@
 const express= require('express');
 const response = require('../../network/response.js');
+const controller = require('./controller.js');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-  console.log(req.headers);
-  res.header({
-    "custom-header": "Nuestro valor personalizado"
-  });
-  response.success(req, res, 'Lista de mensajes',201);
+  controller.getMessages()
+  .then((messageList)=>{
+    response.success(req, res, messageList, 200);
+  })
+  .catch(e => {
+    response.error(req, res, "Unexpected Error", 500, e)
+  })
 });
 router.post('/',(req,res)=>{
-  console.log(req.query.error=="ok");
-  if(req.query.error == "ok"){
-    response.error(req, res, 'Error inesperado',500, 'Es solo una simulacion de los errores');
-  }else{
-    response.success(req, res, 'Creado correctamente',201);
-  }
+  controller.addMessage(req.body.user,req.body.message)
+  .then((fullMessage)=>{
+    response.success(req, res, fullMessage,201);
+  }).catch((e)=>{
+    response.error(req, res, 'Informacion invalida',400, 'Error en el controlador');
+  });
   //res.status(201).send({error: '',body: 'Creado correctamente'});
 
 });
